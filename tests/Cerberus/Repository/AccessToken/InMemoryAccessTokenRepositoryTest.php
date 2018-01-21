b9034b274ebfdc38dabcff3869b99870a4dc5e8c
Tijmen Wierenga <t.wierenga@live.nl>
2018-01-21 13:50:03 +0100

    Implemented logic for revoking access tokens for InMemoryAccessTokenRepository

--- tests/Cerberus/Repository/AccessToken/InMemoryAccessTokenRepositoryTest.php
+++ tests/Cerberus/Repository/AccessToken/InMemoryAccessTokenRepositoryTest.php
@@ -3,6 +3,7 @@ namespace TijmenWierenga\Tests\Cerberus\Repository\AccessToken;
 
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Exception\UniqueTokenIdentifierConstraintViolationException;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 use TijmenWierenga\Cerberus\Repository\AccessToken\InMemoryAccessTokenRepository;
@@ -12,7 +13,7 @@ use TijmenWierenga\Cerberus\Repository\AccessToken\InMemoryAccessTokenRepository
  */
 class InMemoryAccessTokenRepositoryTest extends TestCase
 {
-    public function testItReturnsATokenWithAUserIdentifier()
+    public function testItReturnsATokenWithAUserIdentifier(): AccessTokenEntityInterface
     {
         $repository = new InMemoryAccessTokenRepository();
         $client = $this->getMockBuilder(ClientEntityInterface::class)->getMock();
@@ -25,5 +26,46 @@ class InMemoryAccessTokenRepositoryTest extends TestCase
         $this->assertContains("test", $token->getScopes());
         $this->assertContains("another", $token->getScopes());
         $this->assertEquals($client, $token->getClient());
+
+        return $token;
+    }
+
+    /**
+     * @depends testItReturnsATokenWithAUserIdentifier
+     */
+    public function testItPersistsAToken(AccessTokenEntityInterface $token): AccessTokenEntityInterface
+    {
+        $repository = new InMemoryAccessTokenRepository();
+        $this->assertTrue($repository->isAccessTokenRevoked($token->getIdentifier()));
+
+        $repository->persistNewAccessToken($token);
+        $this->assertFalse($repository->isAccessTokenRevoked($token->getIdentifier()));
+
+        return $token;
+    }
+
+    /**
+     * @depends testItReturnsATokenWithAUserIdentifier
+     */
+    public function testItDoesNotPersistATokenTwice(AccessTokenEntityInterface $token): AccessTokenEntityInterface
+    {
+        $this->expectException(UniqueTokenIdentifierConstraintViolationException::class);
+        $repository = new InMemoryAccessTokenRepository();
+        $repository->persistNewAccessToken($token);
+        $repository->persistNewAccessToken($token);
+    }
+
+    /**
+     * @depends testItReturnsATokenWithAUserIdentifier
+     */
+    public function testItRevokesAToken(AccessTokenEntityInterface $token)
+    {
+        $tokenId = $token->getIdentifier();
+        $repository = new InMemoryAccessTokenRepository();
+        $repository->persistNewAccessToken($token);
+
+        $this->assertFalse($repository->isAccessTokenRevoked($tokenId));
+        $repository->revokeAccessToken($tokenId);
+        $this->assertTrue($repository->isAccessTokenRevoked($tokenId));
     }
 }
