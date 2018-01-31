386b6834e13d6b078ef3d154301918c5890ddc17
Tijmen Wierenga <t.wierenga@live.nl>
2018-01-31 15:47:45 +0100

    Added Scope entity and created InMemoryRepository for testing purposes

---
+++ src/Cerberus/Repository/Scope/InMemoryScopeRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace TijmenWierenga\Cerberus\Repository\Scope;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+use League\OAuth2\Server\Repositories\ScopeRepositoryInterface;
+
+/**
+ * @author Tijmen Wierenga <tijmen.wierenga@devmob.com>
+ */
+class InMemoryScopeRepository implements ScopeRepositoryInterface
+{
+    /**
+     * @var Collection
+     */
+    private $collection;
+
+    /**
+     * InMemoryScopeRepository constructor.
+     * @param Collection $collection
+     */
+    public function __construct(Collection $collection = null)
+    {
+        $this->collection = $collection ?? new ArrayCollection();
+    }
+
+    /**
+     * Return information about a scope.
+     *
+     * @param string $identifier The scope identifier
+     *
+     * @return ScopeEntityInterface
+     */
+    public function getScopeEntityByIdentifier($identifier): ScopeEntityInterface
+    {
+        $result = $this->collection->filter(function (ScopeEntityInterface $scope) use ($identifier) {
+            return $scope->getIdentifier() === $identifier;
+        });
+
+        return $result->first();
+    }
+
+    /**
+     * Given a client, grant type and optional user identifier validate the set of
+     * scopes requested are valid and optionally append additional scopes or remove requested scopes.
+     *
+     * @param ScopeEntityInterface[] $scopes
+     * @param string $grantType
+     * @param ClientEntityInterface $clientEntity
+     * @param null|string $userIdentifier
+     *
+     * @return ScopeEntityInterface[]
+     */
+    public function finalizeScopes(
+        array $scopes,
+        $grantType,
+        ClientEntityInterface $clientEntity,
+        $userIdentifier = null
+    ) {
+        return $scopes;
+    }
+}
---
+++ src/Cerberus/Scope.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TijmenWierenga\Cerberus;
+
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+
+/**
+ * @author Tijmen Wierenga <tijmen.wierenga@devmob.com>
+ */
+class Scope implements ScopeEntityInterface
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * Scope constructor.
+     * @param $id
+     */
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Get the scope's identifier.
+     *
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return (string) $this->id;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize(): string
+    {
+        return $this->id;
+    }
+}
