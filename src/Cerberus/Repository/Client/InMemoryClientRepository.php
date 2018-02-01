edceda732824e5295a68bcc54f762a6c49d13682
Tijmen Wierenga <t.wierenga@live.nl>
2018-02-01 15:21:27 +0100

    InMemoryClientRepository can now validate secrets

--- src/Cerberus/Repository/Client/InMemoryClientRepository.php
+++ src/Cerberus/Repository/Client/InMemoryClientRepository.php
@@ -5,7 +5,9 @@ namespace TijmenWierenga\Cerberus\Repository\Client;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
+use TijmenWierenga\Cerberus\Client;
 
 /**
  * @author Tijmen Wierenga <tijmen.wierenga@devmob.com>
@@ -35,23 +37,38 @@ class InMemoryClientRepository implements ClientRepositoryInterface
      * @param bool $mustValidateSecret If true the client must attempt to validate the secret if the client
      *                                        is confidential
      *
-     * @return ClientEntityInterface
+     * @return ClientEntityInterface|null
      */
     public function getClientEntity(
         $clientIdentifier,
         $grantType,
         $clientSecret = null,
         $mustValidateSecret = true
-    ): ClientEntityInterface {
-        return $this->getClient($clientIdentifier);
-    }
+    ): ?ClientEntityInterface {
+        // TODO: Validate grant type
+        $client = $this->getClient($clientIdentifier);
 
-    private function getClient(string $id): ClientEntityInterface
-    {
-        $client = $this->collection->filter(function (ClientEntityInterface $client) use ($id) {
-            return $client->getIdentifier() === $id;
-        })->first(); // TODO: Check for failure. Id might not exist.
+        if (! $client) {
+            return null;
+        }
+
+        if ($mustValidateSecret && ! $client->validateSecret($clientSecret)) {
+            return null;
+        }
 
         return $client;
+    }
+
+    private function getClient(string $id): ?Client
+    {
+        $result = $this->collection->filter(function (ClientEntityInterface $client) use ($id) {
+            return $client->getIdentifier() === $id;
+        });
+
+        if ($result->isEmpty()) {
+            return null;
+        }
+
+        return $result->first();
     }
 }
