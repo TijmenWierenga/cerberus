601cac59422d31ddff6a83d76ea69766455734d6
Tijmen Wierenga <twieren0@xs4all.net>
2018-02-06 18:00:48 +0100

    Added InMemoryUserRepository. Still have to add hashing passwords

---
+++ tests/Cerberus/Repository/User/InMemoryUserRepositoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace TijmenWierenga\Tests\Cerberus\Repository\User;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use TijmenWierenga\Cerberus\Repository\User\InMemoryUserRepository;
+use TijmenWierenga\Cerberus\User;
+
+class InMemoryUserRepositoryTest extends TestCase
+{
+    public function testItReturnsAUserByUsernameAndPassword(): InMemoryUserRepository
+    {
+        $userId = Uuid::uuid4();
+        $user = User::new($userId, 'tijmen', 'password');
+        $client = $this->getMockBuilder(ClientEntityInterface::class)->getMock();
+        $repo = new InMemoryUserRepository(new ArrayCollection([$user]));
+        $user = $repo->getUserEntityByUserCredentials('tijmen', 'password', 'password', $client);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($userId, $user->getIdentifier());
+
+        return $repo;
+    }
+
+    /**
+     * @depends testItReturnsAUserByUsernameAndPassword
+     * @param InMemoryUserRepository $repo
+     * @return InMemoryUserRepository
+     */
+    public function testItReturnsFalseIfUserCannotBeFound(InMemoryUserRepository $repo): InMemoryUserRepository
+    {
+        $client = $this->getMockBuilder(ClientEntityInterface::class)->getMock();
+        $this->assertFalse($repo->getUserEntityByUserCredentials('henk', 'password', 'password', $client));
+
+        return $repo;
+    }
+
+    /**
+     * @depends testItReturnsFalseIfUserCannotBeFound
+     * @param InMemoryUserRepository $repo
+     */
+    public function testItReturnsFalseIsPasswordIsWrong(InMemoryUserRepository $repo)
+    {
+        $client = $this->getMockBuilder(ClientEntityInterface::class)->getMock();
+
+        $this->assertFalse($repo->getUserEntityByUserCredentials('tijmen', 'false-password', 'password', $client));
+    }
+}
