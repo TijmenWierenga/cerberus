97c011762ece03aa6e3d70986d4c4fb7af2052f7
TijmenWierenga <t.wierenga@live.nl>
2018-05-02 08:32:06 +0200

    Created JSON request parser to have a parsed body in a PSR-7 request

---
+++ src/Middleware/JsonRequestParser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Cerberus\Middleware;
+
+use Cerberus\Exception\InvalidRequestException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class JsonRequestParser implements EventSubscriberInterface
+{
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2')))
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::REQUEST => "parseRequest"];
+    }
+
+    public function parseRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if ($request->getContentType() === "json" && $request->isMethod('POST')) {
+            $result = json_decode($request->getContent(), true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw InvalidRequestException::invalidJson();
+            }
+
+            $request->request->replace($result);
+        }
+    }
+}
