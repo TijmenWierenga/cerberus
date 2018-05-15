3687db37e2dec259971f5e5e47708e94ecbd3200
TijmenWierenga <t.wierenga@live.nl>
2018-05-15 19:16:12 +0200

    Added possibility to add scopes to a user

--- src/OAuth/User.php
+++ src/OAuth/User.php
@@ -2,10 +2,15 @@
 
 namespace Cerberus\OAuth;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
 use League\OAuth2\Server\Entities\UserEntityInterface;
 use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class User implements UserEntityInterface
+class User implements UserEntityInterface, UserInterface
 {
     /**
      * @var UuidInterface
@@ -21,21 +26,21 @@ class User implements UserEntityInterface
     private $password;
 
     /**
-     * User constructor.
-     * @param UuidInterface $id
-     * @param string $username
-     * @param string $password
+     * @var ArrayCollection|ScopeEntityInterface[]
      */
-    private function __construct(UuidInterface $id, string $username, string $password)
+    private $scopes;
+
+    private function __construct(UuidInterface $id, string $username, string $password, array $scopes)
     {
         $this->id = $id;
         $this->username = $username;
         $this->password = $password;
+        $this->scopes = new ArrayCollection($scopes);
     }
 
-    public static function new(UuidInterface $id, string $username, string $password): self
+    public static function new(UuidInterface $id, string $username, string $password, array $scopes = []): self
     {
-        return new self($id, $username, $password);
+        return new self($id, $username, $password, $scopes);
     }
 
     /**
@@ -62,5 +67,88 @@ class User implements UserEntityInterface
     public function getPassword(): string
     {
         return $this->password;
+    }
+
+    public function getScopes(): Collection
+    {
+        return $this->scopes;
+    }
+
+    public function addScope(ScopeEntityInterface $scope, ScopeEntityInterface ...$scopes): void
+    {
+        $scopes[] = $scope;
+
+        foreach ($scopes as $scope) {
+            if (! $this->hasScope($scope)) {
+                $this->scopes->add($scope);
+            }
+        }
+    }
+
+    public function removeScope(ScopeEntityInterface $scope, ScopeEntityInterface ...$scopes): void
+    {
+        $scopes[] = $scope;
+
+        foreach ($scopes as $scope) {
+            if ($this->hasScope($scope)) {
+                $this->scopes->removeElement($scope);
+            }
+        }
+    }
+
+    public function hasScope(ScopeEntityInterface $scope): bool
+    {
+        return $this->scopes->contains($scope);
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        $roles = [];
+
+        foreach ($this->scopes as $scope) {
+            $role = new Role("ROLE_" . strtoupper($scope->getIdentifier()));
+            $roles[] = $role;
+        }
+
+        return $roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        $this->password = "";
     }
 }
---
+++ tests/Unit/OAuth/UserTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Cerberus\Tests\Unit\OAuth;
+
+use Cerberus\OAuth\Scope;
+use Cerberus\OAuth\User;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Security\Core\Role\Role;
+
+class UserTest extends TestCase
+{
+    public function testItInstantiatesAUser()
+    {
+        $id = Uuid::uuid4();
+        $user = User::new($id, 'tijmen', 'a-password');
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals('tijmen', $user->getUsername());
+        $this->assertEquals('a-password', $user->getPassword());
+        $this->assertEmpty($user->getScopes());
+        $this->assertEquals((string) $id, $user->getIdentifier());
+        $this->assertEmpty($user->getRoles());
+    }
+
+    public function testItInstantiatesAUserWithScopes()
+    {
+        [$first, $second] = $scopes =  [
+            new Scope('client_create'),
+            new Scope('client_update')
+        ];
+        $user = User::new(Uuid::uuid4(), 'john', 'a-password', $scopes);
+
+        $this->assertEmpty(array_diff($scopes, $user->getScopes()->toArray()));
+        $this->assertTrue($user->hasScope($first));
+        $this->assertTrue($user->hasScope($second));
+
+        $roles = array_map(function (Role $role) {
+            return $role->getRole();
+        }, $user->getRoles());
+
+        $this->assertEmpty(array_diff(['ROLE_CLIENT_CREATE', 'ROLE_CLIENT_UPDATE'], $roles));
+    }
+
+    public function testItAddsAScopeToAUser()
+    {
+        $user = User::new(Uuid::uuid4(), 'john', 'a-password');
+
+        $this->assertEmpty($user->getScopes());
+
+        $scope = new Scope('client_delete');
+        $user->addScope($scope);
+
+        $this->assertTrue($user->hasScope($scope));
+        $this->assertContains($scope, $user->getScopes());
+
+        [$first, $second ] = $newScopes = [
+            new Scope('user_create'),
+            new Scope('user_remove')
+        ];
+
+        $user->addScope(...$newScopes);
+
+        $this->assertTrue($user->hasScope($first));
+        $this->assertTrue($user->hasScope($second));
+        // Reassure user also has the original added scope
+        $this->assertTrue($user->hasScope($scope));
+    }
+
+    public function testItRemovesAScopeFromAUser()
+    {
+        [$first, $second, $third] = $scopes =  [
+            new Scope('client_create'),
+            new Scope('client_update'),
+            new Scope('user_create')
+        ];
+        $user = User::new(Uuid::uuid4(), 'john', 'a-password', $scopes);
+
+        $this->assertContains($first, $user->getScopes());
+
+        $user->removeScope($first);
+
+        $this->assertFalse($user->hasScope($first));
+        $this->assertTrue($user->hasScope($second));
+        $this->assertTrue($user->hasScope($third));
+
+        $user->removeScope($second, $third);
+
+        $this->assertFalse($user->hasScope($second));
+        $this->assertFalse($user->hasScope($third));
+    }
+}
