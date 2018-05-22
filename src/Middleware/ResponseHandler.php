9507bc709f4326026bb0d306a6bb51c28523b48f
TijmenWierenga <t.wierenga@live.nl>
2018-05-22 08:28:06 +0200

    Added test for ResponseHandler

--- src/Middleware/ResponseHandler.php
+++ src/Middleware/ResponseHandler.php
@@ -3,7 +3,6 @@
 namespace Cerberus\Middleware;
 
 use Cerberus\Response\ResourceResponse;
-use League\Fractal\Manager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
@@ -11,16 +10,6 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class ResponseHandler implements EventSubscriberInterface
 {
-    /**
-     * @var Manager
-     */
-    private $manager;
-
-    public function __construct(Manager $manager)
-    {
-        $this->manager = $manager;
-    }
-
     public static function getSubscribedEvents()
     {
         return [
@@ -31,7 +20,7 @@ class ResponseHandler implements EventSubscriberInterface
     public function onControllerReturn(GetResponseForControllerResultEvent $event)
     {
         $request = $event->getRequest();
-        $contentType = $request->headers->has('Content-Type') ? $request->headers->get('Content-Type') : null;
+        $contentType = $request->headers->has('Accept') ? $request->headers->get('Accept') : null;
         $result = $event->getControllerResult();
 
         if ($result instanceof ResourceResponse) {
