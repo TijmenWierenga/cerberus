46ea3e59c06249fdd9fb807e4c2c885da837033b
TijmenWierenga <t.wierenga@live.nl>
2018-06-06 08:46:20 +0200

    Added integration tests for Client endpoints

--- tests/Integration/ClientTest.php
+++ tests/Integration/ClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Cerberus\Tests\Integration;
 
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClientTest extends BaseTest
@@ -21,6 +22,57 @@ class ClientTest extends BaseTest
 
         $response = $this->client->getResponse();
         $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-        // TODO: Add assertions to check if Client is in database
+
+        $clientId = json_decode($response->getContent(), true)['data']['client']['data']['id'];
+
+        return $clientId;
+    }
+
+    public function testClientCreateInvalidRequestMissingName()
+    {
+        $this->loginWithScopes('client_create');
+
+        $this->client->request('POST', '/api/client', [
+            'grant_types' => ['password', 'client_credentials'],
+            'redirect_uris' => [
+                'https://www.redirect.me/callback',
+                'https://google.com/callback'
+            ]
+        ]);
+
+        $response = $this->client->getResponse();
+        $body = json_decode($response->getContent(), true);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        $this->assertArrayHasKey('name', $body['errors']);
+        $this->assertEquals("Validation failed", $body['message']);
+        $this->assertContains("This field is missing.", $body['errors']['name']['errors']);
+    }
+
+    /**
+     * @depends testClientCreate
+     */
+    public function testItFindsAClientById(string $clientId)
+    {
+        $this->loginWithScopes('client_read');
+
+        $this->client->request('GET', "/api/client/{$clientId}");
+
+        $response = $this->client->getResponse();
+        $body = json_decode($response->getContent(), true);
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('oauth-client', $body['data']['name']);
+        $this->assertEquals($clientId, $body['data']['id']);
+    }
+
+    public function testItReturnsA404WhenClientIsNotFound()
+    {
+        $this->loginWithScopes('client_read');
+        $randomUuid = Uuid::uuid4();
+        $this->client->request('GET', "/api/client/{$randomUuid}");
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
     }
 }
