f972349c3405235492f8254d85d3fe3c97d1028c
TijmenWierenga <t.wierenga@live.nl>
2018-08-21 08:57:18 +0200

    Added extra assertions to verify user was persisted successfully

--- tests/Integration/UserTest.php
+++ tests/Integration/UserTest.php
@@ -3,13 +3,17 @@
 namespace Cerberus\Tests\Integration;
 
 use Cerberus\OAuth\Repository\Scope\ScopeRepositoryInterface;
+use Cerberus\OAuth\Repository\User\UserRepositoryInterface;
 use Cerberus\OAuth\Scope;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Role\Role;
 
 class UserTest extends BaseTest
 {
     public function testUserCreate()
     {
+        /** @var UserRepositoryInterface $userRepository */
+        $userRepository = self::$container->get('test.' . UserRepositoryInterface::class);
         /** @var ScopeRepositoryInterface $scopeRepository */
         $scopeRepository = self::$container->get('test.' . ScopeRepositoryInterface::class);
         $scopeRepository->save(new Scope('client_create'));
@@ -26,5 +30,10 @@ class UserTest extends BaseTest
 
         $response = $this->client->getResponse();
         $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+
+        $userId = json_decode($response->getContent(), true)["data"]["id"];
+        $user = $userRepository->find($userId);
+        $this->assertEquals('tijmen', $user->getUsername());
+        $this->assertTrue($user->hasRole(new Role("ROLE_CLIENT_CREATE")));
     }
 }
